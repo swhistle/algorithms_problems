3eb50439ad5f1d2602219593d5e595fb645d64c8
Alexander S <alexsvistunov73@gmail.com>
2024-06-21 20:00:06 +0300

    add another solution

--- medium/array_string/reverse_words_in_str.py
+++ medium/array_string/reverse_words_in_str.py
@@ -1,8 +1,19 @@
 def reverse_words_in_str(str_: str) -> str:
     words = str_.strip().split(' ')
+    words_reversed = ''
+
+    for i in range(len(words) - 1, -1, -1):
+        if words[i] != '' and words[i] != ' ':
+            words_reversed += ' ' + words[i]
+
+    return words_reversed.strip()
+
+def reverse_words_in_str_2(str_: str) -> str:
+    words = str_.strip().split(' ')
+
     filtered_spaces_words = list(filter(lambda word: word != '', words))
-    print(filtered_spaces_words)
     filtered_spaces_words.reverse()
+
     return ' '.join(filtered_spaces_words).strip()
 
 
